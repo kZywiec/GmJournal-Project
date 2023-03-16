9891bf5551f88f98aa03c32054c5ad687c4d4940
kZywiecPC <92109351+kZywiec@users.noreply.github.com>
2023-03-16 13:02:58 +0100

    Implemented interface and class for RepositoryBase

---
+++ ApplicationCore/Repositories/IRepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTT.Data.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+        Task<bool> ExistsByIdAsync(long id);
+
+        Task<TEntity> GetByIdAsync(long id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        Task UpdateAsync(TEntity entity);
+        Task DeleteByIdAsync(long id);
+        Task DeleteAsync(TEntity entity);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ ApplicationCore/Repositories/RepositoryBase.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VTT.Data.Configuration;
+
+namespace VTT.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+        private readonly VTTDbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public RepositoryBase(VTTDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+
+
+        public async virtual Task<bool> ExistsByIdAsync(long id)
+        {
+            return await _dbSet.AnyAsync(e => e.Id == id);
+        }
+
+        public async virtual Task<TEntity> GetByIdAsync(long id)
+        {
+            var entity = await _dbSet.SingleOrDefaultAsync(e => e.Id == id);
+            
+            if (entity == null)
+                throw new Exception($"Object of type {typeof(TEntity)} with id {id} not found.");
+
+            return entity;
+        }
+
+        public async virtual Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async virtual Task AddAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await SaveChangesAsync();
+        }
+
+        public async virtual Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _dbSet.AddRange(entities);
+            await SaveChangesAsync();
+        }
+
+        public async virtual Task UpdateAsync(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteByIdAsync(long id)
+        {
+            var entity = await GetByIdAsync(id);
+            _dbSet.Remove(entity);
+            await SaveChangesAsync();
+        }
+
+        public async virtual Task DeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await SaveChangesAsync();
+        }
+
+        public async virtual Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
