dbc9f774ed44878d58bc91a7cb5617f085ac28a6
kZywiecPC <92109351+kZywiec@users.noreply.github.com>
2023-03-16 18:19:59 +0100

    Clear WebApp project

---
+++ GmJournal.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GmJournal.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
